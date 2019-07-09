6aa2ba188e5bee47d3aff7c6512f743007a583e6
Angel Anton <hello@monday8am.com>
2019-07-09 21:01:16 +0200

    Changed JSON url

--- app/src/main/java/com/monday8am/checkout/data/remote/RemoteWebService.kt
+++ app/src/main/java/com/monday8am/checkout/data/remote/RemoteWebService.kt
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit
 data class ProductDTO(val products: List<Product>)
 
 interface RemoteWebService {
-    @GET("/bins/4bwec/")
+    @GET("/bins/9d4a7/")
     fun listProducts(): Observable<ProductDTO>
 }
 
